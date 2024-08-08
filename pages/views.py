775631b75524ba4bd8af5c540461c246c8103c40
sivasriraj43 <shivasriraj43@gmail.com>
2024-08-08 12:35:12 +0530

    personal

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home_page_view(request):
+    return HttpResponse("Hello,World")
+
+def about_page_view(request):
+    context = {
+        "name":"Sivasriraj",
+        "age":22,
+    }
+    return render(request,'pages/about.html',context)
